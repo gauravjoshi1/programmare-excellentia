87cb04c2400271d1f90f0a0b28f5e658decd51bb
Puru Jaiswal <p96jcodes@gmail.com>
2025-12-13 19:01:13 -0800

    sort colors

---
+++ Prep/google/sort_colors.cc
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0;
+        int l = 0;
+        int r = nums.size() - 1;
+        while (i <= r) {
+            if (nums[i] == 0) {
+                swap(nums[i], nums[l]);
+                i++;
+                l++;
+            } else if (nums[i] == 2) {
+                swap(nums[i], nums[r]);
+                r--;
+            } else {
+                i++;
+            }
+        }
+    }
+};
