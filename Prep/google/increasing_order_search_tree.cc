0d73c92271672a737cae909ec278b6e685f0368a
Puru Jaiswal <p96jcodes@gmail.com>
2025-12-15 00:53:51 -0800

    increasing order search tree

---
+++ Prep/google/increasing_order_search_tree.cc
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* increasingBST(TreeNode* root) {
+        stack<TreeNode*> st;
+        TreeNode* res = new TreeNode(-1);
+        TreeNode* temp = res;
+        while (root || !st.empty()) {
+            while (root) {
+                st.push(root);
+                root = root->left;
+            }
+            root = st.top();
+            st.pop();
+            res->right = new TreeNode(root->val);
+            res = res->right;
+            root = root->right;
+        }
+        return temp->right; 
+    }
+};
