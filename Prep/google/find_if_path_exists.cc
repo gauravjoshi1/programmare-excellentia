207a681f98206000e49c03d378ca18399356d99e
Puru Jaiswal <p96jcodes@gmail.com>
2025-12-18 01:26:54 -0800

    find if path exists

---
+++ Prep/google/find_if_path_exists.cc
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    std::unordered_map<int, vector<int>> umap;
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        for (int i = 0; i < edges.size(); i++) {
+            umap[edges[i][0]].push_back(edges[i][1]);
+            umap[edges[i][1]].push_back(edges[i][0]);
+        }
+
+        vector<int> visited(n, 0);
+        dfs(umap, source, visited);
+        return visited[destination];
+    }
+
+    void dfs(std::unordered_map<int, vector<int>>& umap, int src, vector<int>& visited) {
+        if (visited[src]) return;
+        visited[src] = true;
+        for (int i = 0; i < umap[src].size(); i++) {
+            dfs(umap, umap[src][i], visited);
+        }
+    }
+};
