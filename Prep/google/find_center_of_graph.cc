204ff4408203654c4b2462f22f46176816837f48
Puru Jaiswal <p96jcodes@gmail.com>
2025-12-18 01:10:47 -0800

    find center of graph

---
+++ Prep/google/find_center_of_graph.cc
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        std::unordered_map<int, vector<int>> umap;
+        for (int i = 0; i < edges.size(); i++) {
+            umap[edges[i][0]].push_back(edges[i][1]);
+            umap[edges[i][1]].push_back(edges[i][0]);
+        }
+
+        for (auto& [k, v] : umap) {
+            if (v.size() == edges.size()) return k;
+        }
+        return -1;
+    }
+};
+
+
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int a = edges[0][0];
+        int b = edges[0][1];
+        int c = edges[1][0];
+        int d = edges[1][1];
+        return (a == c || b == c) ? c : d;
+    }
+};
